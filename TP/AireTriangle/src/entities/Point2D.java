6d21f75b4dfec62055bc3034ed95e60459b103bd
Emmanuel Vankerkore <emmanuel80400@gmail.com>
2022-03-27 13:45:07 +0200

    TP - AireTriangle - Ajout de la documentation pour la classe Point2D

--- TP/AireTriangle/src/entities/Point2D.java
+++ TP/AireTriangle/src/entities/Point2D.java
@@ -1,14 +1,30 @@
 package entities;
 
+/**
+* @author Emmanuel Vankerkore
+* @date 27/03/2022
+* 
+* La classe Point2D a pour but de définir n'importe quel point de deux dimensions.
+*
+*/
+
 public class Point2D {
 	
 	String nom;
 	Double x;
 	Double y;
 	
-	public Point2D(String nom, Double abcisse, Double ordonnee) {
+	/**
+	 * On définit notre constructeur de notre classe:
+	 * 
+	 * @param nom : un nom que l'on donnera à notre point.
+	 * @param abscisse : l'entier correspondant à l'abscisse de notre point.
+	 * @param ordonnee : l'entier correspondant à l'ordonnée de la droite affine
+	 */
+	
+	public Point2D(String nom, Double abscisse, Double ordonnee) {
 		setNom(nom);
-		setX(abcisse);
+		setX(abscisse);
 		setY(ordonnee);
 	}
 
@@ -36,6 +52,10 @@ public class Point2D {
 		this.y = y;
 	}
 
+	/**
+	 * La méthode affichera toutes les informations assoiciées à l'objet.
+	 */
+	
 	public void afficher() {
 		System.out.println(getNom() + " --> (" + getX() + " ; " + getY() + ")");
 	}
