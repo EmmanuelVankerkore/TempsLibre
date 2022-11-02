db3dbcf5df9189f974df4a753fb377144150d8fd
Emmanuel Vankerkore <emmanuel80400@gmail.com>
2022-11-02 15:55:41 +0100

    TP EstimationPi Programme fonctionnel mais résultats érronés (RAF : Documenter, Enlever code inutile, Revérifier)

--- TP/EstimationPi/src/run/Run.java
+++ TP/EstimationPi/src/run/Run.java
@@ -1,6 +1,6 @@
 package run;
 
-import calcul.Calcul;
+
 import entities.DroiteAffine;
 import entities.Point2D;
 import entities.Segment;
@@ -9,53 +9,45 @@ import resultats.Record;
 
 
 public class Run {
+	
+	static Point2D pPermanent = new Point2D("permanent", 1.0, 0.0);
+	static Point2D pCentreCercle = new Point2D("Centre", 0.0, 0.0);
+	
 	public static void main(String[] args) {
-		//System.out.println("Bonjour");
 		ListRecords liRe = new ListRecords();
-		
-		Point2D centreCercle = new Point2D("Centre", 0.0, 0.0);
-		Point2D pointTemp = new Point2D("t", 0.0, 1.0);
-		Point2D pointPerm = new Point2D("permanent", 1.0, 0.0);
-		
-		// 4. Définir Segment co tangeant au cercle
-		
-		Segment segmentInitiale = new Segment("calcules moi", pointTemp, pointPerm);
-		
-		// 5. Calculer la taille
-		
-		Double valeurApproxPi = segmentInitiale.getTaille();
-		//System.out.println(valeurApproxPi);
-		//affichage.Affichage.afficherValeurFourchette(valeurApproxPi, 7);
-		//System.out.println(Calcul.arrondirAuInf(valeurApproxPi, 7));
-		//System.out.println(Calcul.arrondirAuSup(valeurApproxPi, 7));
-		
-		// 6. Créer la séquence
-		
-		Record r = new Record(valeurApproxPi, 2);
-		
-		// 7. Stocket la séquence avec les autre
-		
-		liRe.ajouterRecord(r);
+		Point2D pHautSegment = new Point2D("t", 0.0, 1.0);
+		liRe = getToutLesEnregistrements(liRe, pHautSegment, 2, 1, 12);
 		liRe.afficher();
-		
-		// 1. Trouver le point du centre du segment
-		
-		Point2D milieu = segmentInitiale.getPointDuMilieu();
-		//milieu.afficher();
-		
-		// 2. Définir la droite affine perpendiculare
-		
-		DroiteAffine dCentre = new DroiteAffine("droitePassantParCentre", centreCercle, milieu);
-		//dCentre.afficher();
-		
-		// 3. Définir les points ( approx ) communs entre la la droite et le cercle
-		
-		pointTemp = dCentre.getPointDuCercle();
-		//System.out.println(pointTemp.estQuasiSurCercle());
-		//System.out.println(milieu.estQuasiSurCercle());
 	}
 	
+	public static Point2D getNouveauPoint(Point2D pHautSegment) {
+		Segment sInitiale = new Segment("SegmentTailleDejaConnue", pHautSegment, pPermanent);
+		Point2D pmilieu = sInitiale.getPointDuMilieu();
+		DroiteAffine dCentre = new DroiteAffine("droitePassantParCentre", pCentreCercle, pmilieu);
+		return dCentre.getPointDuCercle();
+	}
 	
+	public static Record getNouvelEnregistrement(Point2D pHautSegment, int coefMult) {
+		Segment sFinal = new Segment("SegmentTailleInconnue", pHautSegment, pPermanent);
+		Double valeurApproxPi = sFinal.getTaille();
+		return new Record(valeurApproxPi, coefMult);
+	}
+	
+	public static ListRecords getToutLesEnregistrements(ListRecords oldListeRec, 
+														Point2D pDepart,
+														int coefMultSegment,
+														int Iteration, 
+														int nbIterations) {
+		if (Iteration <= nbIterations) {
+			ListRecords newListeRec = oldListeRec;
+			Point2D pNewDepart = getNouveauPoint(pDepart);
+			newListeRec.ajouterRecord(getNouvelEnregistrement(pNewDepart, coefMultSegment));
+			return getToutLesEnregistrements(newListeRec, pNewDepart, coefMultSegment*2, Iteration+1, nbIterations);
+		} 
+		else {
+			return oldListeRec;
+		}
+	}
 	
 	
 }
