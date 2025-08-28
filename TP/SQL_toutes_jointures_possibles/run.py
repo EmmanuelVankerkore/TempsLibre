e960c28ba8065200b85dc351d663220c89a74d97
Emmanuel Vankerkore <emmanuel80400@gmail.com>
2025-08-28 21:32:33 +0200

    ajout partiel des variables globales

--- TP/SQL_toutes_jointures_possibles/run.py
+++ TP/SQL_toutes_jointures_possibles/run.py
@@ -1,5 +1,10 @@
 
 
+pathfile_in = 'C:\\Dev\\Comptage_Force_Filtre\\Data\\Input\\RequeteSQL_balises.sql'
+pathfile_out = 'C:\\Dev\\Comptage_Force_Filtre\\Data\\Input\\RequeteSQL_balises.sql'
+expression_prefixe = 'round( (count(1) - count(case when' 
+expression_suffixe = 'then 0 end )) / count(1) * 100) as population_restante_filtre_'
+
 def main() -> None:
     pass
 
